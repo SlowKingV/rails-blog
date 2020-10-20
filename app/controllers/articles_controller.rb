7254b49b5e83416d036d37a9a4737d1b529dd0c4
SlowKingV <slowkingvii@gmail.com>
2020-10-20 17:01:15 -0500

    :sparkles: Add '#edit' and '#update' methods, add flash notice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' has been created successfully!"
     redirect_to article_path(@article)
   end
 
@@ -24,6 +25,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
+    flash.notice = "Article '#{@article.title}' has been deleted permanently!"
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
   end
 end
